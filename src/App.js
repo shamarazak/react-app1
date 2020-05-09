9d7ed096a7d9c5a57cd2ff2d37169e3a63974e39
shamarazak <shamarazak@gmail.com>
2020-05-09 23:24:56 +0530

    Components part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import{ Navbar, NavbarBrand } from 'reactstrap'
 import Menu  from "./components/MenuComponent";
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component{
+
+  constructor (props){
+  super(props);
+  this.state = {
+    dishes:DISHES
+  }
+  }
+  render(){
   return (
     <div>
      <Navbar dark color="primary">
@@ -12,9 +21,9 @@ function App() {
          <NavbarBrand href="/">shama razak</NavbarBrand>
        </div>
      </Navbar>
-     <Menu />
+     <Menu dishes={this.state.dishes} />
     </div>
   );
 }
-
+}
 export default App;
